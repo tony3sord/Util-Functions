6debd7681d9bcd6e6c7e4d9216a71d91ff5044bb
tony3sord <133953601+tony3sord@users.noreply.github.com>
2023-08-21 23:10:35 +0200

    Detector de dispositivo.js

---
+++ Detector de dispositivo.js
@@ -0,0 +1,30 @@
+function userDeviceInfo() {
+    const isMobile={
+        android:()=>ua.match(/android/i),
+        ios:()=>ua.match(/iphone|ipad|ipod/i),
+        windows:()=>ua.match(/windows phone/i),
+        any: function (){
+            return this.android() || this.ios() || this.windows();
+        }
+    };
+    const isDesktop={
+        linux:()=>ua.match(/linux/i),
+        mac:()=>ua.match(/mac os/i),
+        windows:()=>ua.match(/windows nt/i),
+        any: function (){
+            return this.linux() || this.mac() || this.windows();
+        }
+    };
+    const isBrowser={
+        chrome:()=>ua.match(/chrome/i),
+        safari:()=>ua.match(/safari/i),
+        firefox:()=>ua.match(/firefox/i),
+        opera:()=>ua.match(/opera|opera mini/i),
+        ie:()=>ua.match(/msie|iemobile/i),
+        edge:()=>ua.match(/edge /i),
+        any: function (){
+            return (this.chrome() || this.safari() || this.firefox||
+            this.opera() || this.ie() || this.edge);
+        }
+    };
+}
