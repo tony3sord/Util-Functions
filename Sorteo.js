3a5da5d28204d5dfd7d154bf082646a5985cbe18
tony3sord <133953601+tony3sord@users.noreply.github.com>
2023-08-22 21:38:34 +0200

    Sorteo.js

---
+++ Sorteo.js
@@ -0,0 +1,17 @@
+const d=document;
+
+export function sortear(selector,btn) {
+    const random = (selector)=>{
+        const arr=d.querySelectorAll(selector);
+        let sorteo=Math.floor(Math.random()*10);
+        const ganador=arr[sorteo];
+        return `El ganador es ${ganador.textContent}`;
+    }
+    d.addEventListener("click",e=>{
+        if(e.target.matches(btn)){
+            let g=random(selector);
+            alert(g);
+            console.log(g);
+        }
+    })
+}
